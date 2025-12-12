b10fa1744d3bcc13815802333eec326454607406
Smriti Dube <smritiiii04@gmail.com>
2025-12-12 13:43:23 +0530

    first commit

---
+++ gRPCClient/src/test/java/com/example/gRPCClient/GRpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gRPCClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gRPCServer/src/test/java/com/example/gRPCServer/GRpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gRPCServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
